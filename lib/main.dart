b5fe9a88682dd86b34b08db2c11e605c13d85e4e
danish <danishalif16@gmail.com>
2025-11-12 19:25:51 +0700

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("MyApp"),
+        title: Text("Home"),
         centerTitle: true,
         backgroundColor: Colors.deepPurple[100],
       ),
       body: Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.black,
-            fontFamily: 'Montserrat',
-          ),
-        ),
+        child: Image.network("https://images.unsplash.com/photo-1762770663487-6c03bb94fe84?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600")
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple[100],
@@ -27,6 +27,6 @@ void main() {
         },
         child: Text("Button"),
       ),
-    ),
-  ));
+    );
+  }
 }
