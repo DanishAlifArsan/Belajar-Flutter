bd288479d6b58ca368a0b09f41e1b33b7f868033
danish <danishalif16@gmail.com>
2025-11-13 14:34:35 +0700

    create list data

--- lib/main.dart
+++ lib/main.dart
@@ -7,82 +7,112 @@ final log = Logger('My App');
 
 void main() {
   runApp(MaterialApp(
-    home: Home()
+    home: QuoteList()
   ));
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
-  int level = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<String> quoteList = [
+    "Hello World",
+    "Hello Flutter",
+    "This is my App",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("Quote List"),
         centerTitle: true,
-        backgroundColor: Colors.deepPurple[ 100],
+        backgroundColor: Colors.deepPurple,
         elevation: 0,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 3,
-              child: Image.network("https://images.unsplash.com/photo-1762912302731-508b4580735f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-          ),
-          Expanded(
-              flex: 1,
-              child: Text("Level $level")
-          ),
-          Expanded(
-            flex: 2,
-            child: FilledButton(
-                onPressed: () {}, child: Text("Button")
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(50),
-              color: Colors.grey[100],
-              child: Text("Container"),
-            ),
-          ),
-        ],
-      ),
-
-
-
-      // Center(
-      //   child: ElevatedButton.icon(
-      //       onPressed: () {
-      //         log.info("Button pressed");
-      //       },
-      //       icon: Icon(Icons.mail),
-      //       label: Text("Mail Me"),
-      //       style: ButtonStyle(
-      //         backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
-      //       ),
-      //   ),
-      // ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.deepPurple[100],
-        onPressed: () {
-          setState(() {
-            level++;
-          });
-        },
-        child: Text("Button"),
+      body: Column(
+        children: quoteList.map((quote) => Text(quote)).toList(),
       ),
     );
   }
 }
 
+// class Home extends StatefulWidget {
+//   const Home({super.key});
+//
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   int level = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home"),
+//         centerTitle: true,
+//         backgroundColor: Colors.deepPurple[ 100],
+//         elevation: 0,
+//       ),
+//       body: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Expanded(
+//             flex: 3,
+//               child: Image.network("https://images.unsplash.com/photo-1762912302731-508b4580735f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+//           ),
+//           Expanded(
+//               flex: 1,
+//               child: Text("Level $level")
+//           ),
+//           Expanded(
+//             flex: 2,
+//             child: FilledButton(
+//                 onPressed: () {}, child: Text("Button")
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Container(
+//               padding: EdgeInsets.all(50),
+//               color: Colors.grey[100],
+//               child: Text("Container"),
+//             ),
+//           ),
+//         ],
+//       ),
+//
+//
+//
+//       // Center(
+//       //   child: ElevatedButton.icon(
+//       //       onPressed: () {
+//       //         log.info("Button pressed");
+//       //       },
+//       //       icon: Icon(Icons.mail),
+//       //       label: Text("Mail Me"),
+//       //       style: ButtonStyle(
+//       //         backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+//       //       ),
+//       //   ),
+//       // ),
+//       floatingActionButton: FloatingActionButton(
+//         backgroundColor: Colors.deepPurple[100],
+//         onPressed: () {
+//           setState(() {
+//             level++;
+//           });
+//         },
+//         child: Text("Button"),
+//       ),
+//     );
+//   }
+// }
