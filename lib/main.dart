d83fed0333f9aa4a20ecf1788494104a7aefb470
danish <danishalif16@gmail.com>
2025-11-13 13:56:56 +0700

    add stateful widget and level indicator

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,15 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int level = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -20,27 +27,62 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home"),
         centerTitle: true,
-        backgroundColor: Colors.deepPurple[100],
+        backgroundColor: Colors.deepPurple[ 100],
+        elevation: 0,
       ),
-      body: Center(
-        child: ElevatedButton.icon(
-            onPressed: () {
-              log.info("Button pressed");
-            },
-            icon: Icon(Icons.mail),
-            label: Text("Mail Me"),
-            style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 3,
+              child: Image.network("https://images.unsplash.com/photo-1762912302731-508b4580735f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+          ),
+          Expanded(
+              flex: 1,
+              child: Text("Level $level")
+          ),
+          Expanded(
+            flex: 2,
+            child: FilledButton(
+                onPressed: () {}, child: Text("Button")
             ),
-        ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(50),
+              color: Colors.grey[100],
+              child: Text("Container"),
+            ),
+          ),
+        ],
       ),
+
+
+
+      // Center(
+      //   child: ElevatedButton.icon(
+      //       onPressed: () {
+      //         log.info("Button pressed");
+      //       },
+      //       icon: Icon(Icons.mail),
+      //       label: Text("Mail Me"),
+      //       style: ButtonStyle(
+      //         backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+      //       ),
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple[100],
-        onPressed: () => {
-
+        onPressed: () {
+          setState(() {
+            level++;
+          });
         },
         child: Text("Button"),
       ),
     );
   }
 }
+
